d399939bc12ed57339d85e431ca7a176a355f0cd
zwelihle comet <zwelihlecomet2@gmail.com>
2025-03-28 15:30:54 +0000

    testing responsiveness

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const track = document.getElementById('slideshowTrack');
-const cards = document.querySelectorAll('.slideshow-card');
-const cardWidth = cards[0].offsetWidth + 20; // Width of each card + padding
+let track = document.getElementById('slideshowTrack');
+let cards = document.querySelectorAll('.slideshow-card');
+let cardWidth = cards[0].offsetWidth + 20; 
 
 let currentIndex = 0;
 
 function slideCards() {
   currentIndex++;
   if (currentIndex >= cards.length) {
-    currentIndex = 0; // Loop back to the start
+    currentIndex = 0; 
   }
   track.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
 }
 
-// Change cards every 3 seconds
 setInterval(slideCards, 1000);
